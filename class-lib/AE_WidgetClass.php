af7715988b2de2f9b7179dc7f07399f8be2b1809
tepelstreel <tepelstreel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-28 10:16:41 +0000

    Ads Easy 3.0 tag
    git-svn-id: https://plugins.svn.wordpress.org/adseasy/tags/3.0@883476 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- class-lib/AE_WidgetClass.php
+++ class-lib/AE_WidgetClass.php
@@ -13,39 +13,42 @@ class Ads_Easy_Widget extends WP_Widget {
 	
 	const language_file = 'adseasy';
 	
+	private static $options;
+	
 	function __construct() {
 			
 		$widget_opts = array( 'description' => __('You can show ads in your sidebars and other widgetareas with this widget. Define, on what kind of pages they will show up.', self::language_file) );
 		$control_opts = array ( 'width' => 400 );
 		
 		parent::WP_Widget(false, $name = 'Ads Easy', $widget_opts, $control_opts);
+		
+		self::$options = get_option('ae_options');
 	
 	}
-		
-		
+
 	function form($instance) {
 		
 		$defaults = array(
-				'title' => NULL,
-				'name' => NULL,
-				'adblock' => NULL,
-				'style' => NULL,
-				'homepage' => 1,
-				'frontpage' => false,
-				'page' => false,
-				'category' => 1,
-				'single' => false,
-				'date' => false,
-				'tag' => false,
-				'attachment' => false,
-				'taxonomy' => false,
-				'author' => false,
-				'search' => false,
-				'not_found' => false,
-				'logged_in' => 1,
-				'search_engine' => 1,
-				'normal' => 1
-				);
+			'title' => NULL,
+			'name' => NULL,
+			'adblock' => NULL,
+			'style' => NULL,
+			'homepage' => 1,
+			'frontpage' => false,
+			'page' => false,
+			'category' => 1,
+			'single' => false,
+			'date' => false,
+			'tag' => false,
+			'attachment' => false,
+			'taxonomy' => false,
+			'author' => false,
+			'search' => false,
+			'not_found' => false,
+			'logged_in' => 1,
+			'search_engine' => 1,
+			'normal' => 1
+		);
 		
 		$instance = wp_parse_args( (array) $instance, $defaults );
 		
@@ -73,19 +76,19 @@ class Ads_Easy_Widget extends WP_Widget {
 		$base_name = 'widget-'.$this->id_base.'['.$this->number.']';
 		
 		$options = array (
-					array($base_id.'homepage', $base_name.'[homepage]', $homepage, __('Homepage', self::language_file)),
-					array($base_id.'frontpage', $base_name.'[frontpage]', $frontpage, __('Frontpage (e.g. a static page as homepage)', self::language_file)),
-					array($base_id.'page', $base_name.'[page]', $page, __('&#34;Page&#34; pages', self::language_file)),
-					array($base_id.'category', $base_name.'[category]', $category, __('Category pages', self::language_file)),
-					array($base_id.'single', $base_name.'[single]', $single, __('Single post pages', self::language_file)),
-					array($base_id.'date', $base_name.'[date]', $date, __('Archive pages', self::language_file)),
-					array($base_id.'tag', $base_name.'[tag]', $tag, __('Tag pages', self::language_file)),
-					array($base_id.'attachment', $base_name.'[attachment]', $attachment, __('Attachments', self::language_file)),
-					array($base_id.'taxonomy', $base_name.'[taxonomy]', $taxonomy, __('Custom Taxonomy pages (only available, if having a plugin)', self::language_file)),
-					array($base_id.'author', $base_name.'[author]', $author, __('Author pages', self::language_file)),
-					array($base_id.'search', $base_name.'[search]', $search, __('Search Results', self::language_file)),
-					array($base_id.'not_found', $base_name.'[not_found]', $not_found, __('&#34;Not Found&#34;', self::language_file))
-			);
+			array($base_id.'homepage', $base_name.'[homepage]', $homepage, __('Homepage', self::language_file)),
+			array($base_id.'frontpage', $base_name.'[frontpage]', $frontpage, __('Frontpage (e.g. a static page as homepage)', self::language_file)),
+			array($base_id.'page', $base_name.'[page]', $page, __('&#34;Page&#34; pages', self::language_file)),
+			array($base_id.'category', $base_name.'[category]', $category, __('Category pages', self::language_file)),
+			array($base_id.'single', $base_name.'[single]', $single, __('Single post pages', self::language_file)),
+			array($base_id.'date', $base_name.'[date]', $date, __('Archive pages', self::language_file)),
+			array($base_id.'tag', $base_name.'[tag]', $tag, __('Tag pages', self::language_file)),
+			array($base_id.'attachment', $base_name.'[attachment]', $attachment, __('Attachments', self::language_file)),
+			array($base_id.'taxonomy', $base_name.'[taxonomy]', $taxonomy, __('Custom Taxonomy pages (only available, if having a plugin)', self::language_file)),
+			array($base_id.'author', $base_name.'[author]', $author, __('Author pages', self::language_file)),
+			array($base_id.'search', $base_name.'[search]', $search, __('Search Results', self::language_file)),
+			array($base_id.'not_found', $base_name.'[not_found]', $not_found, __('&#34;Not Found&#34;', self::language_file))
+		);
 		
 		$checkall = array($base_id.'checkall', $base_name.'[checkall]', __('Check all', self::language_file));
 		
@@ -96,7 +99,7 @@ class Ads_Easy_Widget extends WP_Widget {
 		a5_checkbox($base_id.'search_engine', $base_name.'[search_engine]', $search_engine, __('Show to visitors, who come from search engines.', self::language_file), array('space' => true));
 		a5_checkbox($base_id.'normal', $base_name.'[normal]', $normal, __('Show to other visitors.', self::language_file), array('space' => true));
 		a5_textarea($base_id.'adblock', $base_name.'[adblock]', $adblock, __('Just paste the code of your ad here.', self::language_file), array('space' => true, 'style' => 'height: 60px;', 'class' => 'widefat'));
-		a5_textarea($base_id.'style', $base_name.'[style]', $style, sprintf(__('Here you can finally style the widget. Simply type something like%1$s%2$sborder: 1px solid;%1$sborder-color: #000000;%3$s%1$sto get just a black line around the widget. If you leave that section empty, your theme will style the widget.', self::language_file), '<br />', '<strong>', '</strong>'), array('space' => true, 'style' => 'height: 60px;', 'class' => 'widefat'));
+		if (empty(self::$options['ae_css'])) a5_textarea($base_id.'style', $base_name.'[style]', $style, sprintf(__('Here you can finally style the widget. Simply type something like%1$s%2$sborder: 1px solid;%1$sborder-color: #000000;%3$s%1$sto get just a black line around the widget. If you leave that section empty, your theme will style the widget.', self::language_file), '<br />', '<strong>', '</strong>'), array('space' => true, 'style' => 'height: 60px;', 'class' => 'widefat'));
 		a5_resize_textarea(array($base_id.'adblock', $base_id.'style'), true);
 		
 	}
@@ -160,20 +163,14 @@ class Ads_Easy_Widget extends WP_Widget {
 			$title = apply_filters('widget_title', $instance['name']);	
 			
 			if (empty($instance['style'])) :
-				
-				$ae_before_widget=$before_widget;
-				$ae_after_widget=$after_widget;
 			
-			else :
+				$style=str_replace(array("\r\n", "\n", "\r"), '', $instance['style']);
 				
-				$ae_style=str_replace(array("\r\n", "\n", "\r"), '', $instance['style']);
-				
-				$ae_before_widget='<div id="'.$widget_id.'" style="'.$ae_style.'">';
-				$ae_after_widget='</div>';
-				
+				$before_widget = str_replace('>', 'style="'.$style.'">', $before_widget);
+			
 			endif;
 			
-			echo $ae_before_widget;
+			echo $before_widget;
 			
 			if ( $title ) echo $before_title . $title . $after_title;
 		 
@@ -181,7 +178,7 @@ class Ads_Easy_Widget extends WP_Widget {
 				
 			echo $instance['adblock'];
 			
-			echo $ae_after_widget;
+			echo $after_widget;
 		
 		endif;
 	
