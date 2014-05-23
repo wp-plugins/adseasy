6f77c80e4c2865c6372fada51b9859bfd3f10d14
tepelstreel <tepelstreel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-23 08:28:18 +0000

    AdsEasy 2.9.2
    git-svn-id: https://plugins.svn.wordpress.org/adseasy/tags/2.9.2@919819 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ class-lib/AE_DynamicCSSClass.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ *
+ * Class AE Dynamic CSS
+ *
+ * Extending A5 Dynamic Files
+ *
+ * Presses the dynamical CSS of the Ads Easy Widget into a virtual style sheet
+ *
+ */
+
+class AE_DynamicCSS extends A5_DynamicFiles {
+	
+	private static $options;
+	
+	function __construct() {
+		
+		self::$options =  get_option('ae_options');
+		
+		if (!isset(self::$options['inline'])) self::$options['inline'] = false;
+		
+		if (!array_key_exists('compress', self::$options)) self::$options['compress'] = false;
+		
+		parent::A5_DynamicFiles('wp', 'css', false, self::$options['inline']);
+		
+		$eol = (self::$options['compress']) ? '' : "\r\n";
+		$tab = (self::$options['compress']) ? ' ' : "\t";
+		
+		$css_selector = '[id^="ads_easy_widget"].widget_ads_easy_widget';
+		
+		parent::$wp_styles .= (!self::$options['compress']) ? $eol.'/* CSS portion of Ads Easy */'.$eol.$eol : '';
+		
+		$style=str_replace('; ', ';'.$eol.$tab, str_replace(array("\r\n", "\n", "\r"), ' ', self::$options['ae_css']));
+
+		parent::$wp_styles.='div'.$css_selector.','.$eol.'li'.$css_selector.','.$eol.'aside'.$css_selector.' {'.$eol.$tab.$style.$eol.'}'.$eol;
+
+	}
+	
+} // AE_Dynamic CSS
+
+?>
